4878c6923834abd51cce269fe2819529aef92857
wu979 <18683789594@163.com>
2019-02-27 12:40:39 +0800

    提交

--- ecasin-system/src/main/java/com/csd/system/role/service/RoleService.java
+++ ecasin-system/src/main/java/com/csd/system/role/service/RoleService.java
@@ -268,10 +268,12 @@ public class RoleService extends DeleteService<Role> {
 
     public List<String> addMenuSunByRecursion(List<Menu> menuAll,String menuId,List<String> menuRecursionList){
         for(Menu menu : menuAll){
-            if(menu.getMenuPid().equals(menuId)){
-                menuRecursionList.add(menu.getMenuId());
-                if(menu.getMenuHasChild() == Integer.valueOf(ConstantUtil.CODE_ONE)){
-                    menuRecursionList = addMenuSunByRecursion(menuAll,menu.getMenuId(),menuRecursionList);
+            if(!StringUtil.isEmpty(menu.getMenuPid())) {
+                if (menu.getMenuPid().equals(menuId)) {
+                    menuRecursionList.add(menu.getMenuId());
+                    if (menu.getMenuHasChild() == Integer.valueOf(ConstantUtil.CODE_ONE)) {
+                        menuRecursionList = addMenuSunByRecursion(menuAll, menu.getMenuId(), menuRecursionList);
+                    }
                 }
             }
         }
@@ -283,7 +285,7 @@ public class RoleService extends DeleteService<Role> {
         //根据父级ID 找到所有父级,如果都未被标记则删除父级,递归查询
         for(Menu parentMenu : menuAll){
             if(parentMenu.getMenuId().equals(request.getMenuPid())){
-                menuRecursionList = this.deleteMenuParentByRecursion(menuAll,parentMenu,menuRecursionList);
+                menuRecursionList = this.deleteMenuParentByRecursion(menuAll,parentMenu,menuRecursionList,request.getMenuId());
             }
         }
         //根据菜单ID 找到所有子级,递归查询
@@ -299,22 +301,27 @@ public class RoleService extends DeleteService<Role> {
     }
 
 
-    public List<String> deleteMenuParentByRecursion(List<Menu> menuAll,Menu parentMenu,List<String> menuRecursionList){
+    public List<String> deleteMenuParentByRecursion(List<Menu> menuAll,Menu parentMenu,List<String> menuRecursionList,String menuId){
         int count = 0;
-        Menu resultMenu = null;
         for(Menu menu : menuAll){
-            if(menu.getMenuId().equals(parentMenu.getMenuPid())){
-                if(!StringUtil.isEmpty(menu.getMenuIsNull())){
-                    count ++;
-                }else {
-                    resultMenu = menu;
+            if(!StringUtil.isEmpty(menu.getMenuPid())){
+                if(menu.getMenuPid().equals(parentMenu.getMenuId())){
+                    if(!menu.getMenuId().equals(menuId)){
+                        if(!StringUtil.isEmpty(menu.getMenuIsNull())){
+                            count ++;
+                        }
+                    }
                 }
             }
         }
         if(count == 0){
-            menuRecursionList.add(resultMenu.getMenuId());
-            if(!StringUtil.isEmpty(resultMenu.getMenuPid())){
-                menuRecursionList = deleteMenuParentByRecursion(menuAll,resultMenu,menuRecursionList);
+            menuRecursionList.add(parentMenu.getMenuId());
+            if(!StringUtil.isEmpty(parentMenu.getMenuPid())){
+                for(Menu parentRecursionMenu : menuAll){
+                    if(parentRecursionMenu.getMenuId().equals(parentMenu.getMenuPid())){
+                        menuRecursionList = deleteMenuParentByRecursion(menuAll,parentRecursionMenu,menuRecursionList,parentMenu.getMenuId());
+                    }
+                }
             }
         }
         return menuRecursionList;
@@ -323,10 +330,12 @@ public class RoleService extends DeleteService<Role> {
 
     public List<String> deleteMenuSunByRecursion(List<Menu> menuAll,String menuId,List<String> menuRecursionList){
         for(Menu menu : menuAll){
-            if(menu.getMenuPid().equals(menuId)){
-                menuRecursionList.add(menu.getMenuId());
-                if(menu.getMenuHasChild() == Integer.valueOf(ConstantUtil.CODE_ONE)){
-                    menuRecursionList = deleteMenuSunByRecursion(menuAll,menu.getMenuId(),menuRecursionList);
+            if(!StringUtil.isEmpty(menu.getMenuPid())) {
+                if (menu.getMenuPid().equals(menuId)) {
+                    menuRecursionList.add(menu.getMenuId());
+                    if (menu.getMenuHasChild() == Integer.valueOf(ConstantUtil.CODE_ONE)) {
+                        menuRecursionList = deleteMenuSunByRecursion(menuAll, menu.getMenuId(), menuRecursionList);
+                    }
                 }
             }
         }
